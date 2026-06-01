0047116136167ffdac82a56d95f7aa0a23c9d3e5
Kiran <kiranecherukuri@gmail.com>
2026-06-01 10:19:53 -0400

    Use Inter font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,12 @@
 import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 import type { ReactNode } from "react";
 import "./globals.css";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
 
 export const metadata: Metadata = {
   title: "Palar",
@@ -14,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={inter.variable}>{children}</body>
     </html>
   );
 }
